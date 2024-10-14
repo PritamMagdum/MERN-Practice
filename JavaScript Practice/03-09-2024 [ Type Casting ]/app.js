b553b11b4d70fb62346544d93723d734bdefb8d1
Pritam <pritammagdum24@gmail.com>
2024-10-14 09:40:55 -0700

    Type Casting or Conversion

---
+++ JavaScript Practice/03-09-2024 [ Type Casting ]/app.js
@@ -0,0 +1,15 @@
+// & Type Casting or Conversion
+/* 
+In type conversion there are two types
+    ^ 1) Implicit
+    ^ 2) Explicit
+*/
+// ^ 1] Implicit
+let a = "10" + 10;
+console.log(a);
+console.log(typeof a);
+
+// ^ 2] Explicit
+let b = String(10);
+console.log(b);
+console.log(typeof b);
