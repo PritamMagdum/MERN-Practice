5dd355e771df390d99447bc86f0c3bb858ef6245
Pritam <pritammagdum24@gmail.com>
2024-10-14 08:37:20 -0700

    Typeof Operator

---
+++ JavaScript Practice/01-09-2024 [ typeof Operator ]/app.js
@@ -0,0 +1,35 @@
+// & typeof Operator
+let s = "Pritam";
+console.log(typeof s);
+
+let n = 23;
+console.log(typeof n);
+
+let un = undefined;
+console.log(typeof un);
+
+let nl = null;
+console.log(typeof nl);
+
+let bI = BigInt;
+console.log(typeof bI);
+
+let sy = Symbol;
+console.log(typeof sy);
+
+let t = true;
+console.log(typeof t);
+
+console.log("***************");
+console.log(typeof typeof String());
+console.log(typeof Number);
+console.log(typeof Number());
+
+function demo() {
+  console.log("Function Demo Called");
+}
+console.log(typeof demo);
+console.log(typeof demo());
+
+console.log(typeof typeof Number);
+console.log(typeof null);
