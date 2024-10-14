8915914804bbe3bcd429a120a2280e1411feb59e
Pritam <pritammagdum24@gmail.com>
2024-10-14 08:07:15 -0700

    Printing Methods in JavaScript

---
+++ JavaScript Practice/29-08-2024 [ Printing Methods ]/app.js
@@ -0,0 +1,18 @@
+console.log("Console Methods");
+
+// 1 console.log()
+console.log("Method 1");
+
+// 2. document.write()
+document.write("Method 2");
+
+// 3. alert()
+alert("Method 3");
+
+// 4. confirm()
+let a = confirm("Method 4");
+console.log(a);
+
+// 5. promt()
+let b = prompt("Method 5");
+console.log(b);
