3e187170cbfa6948ed8a0417372e98401bd4118d
Pritam <pritammagdum24@gmail.com>
2024-11-14 22:37:39 +0530

    Solved Distinct Numbers

--- Data Structures Lectures/distinctNumbers.js
+++ Data Structures Lectures/distinctNumbers.js
@@ -11,18 +11,3 @@ let c = 3;
 // } else {
 //   console.log("c is middle");
 // }
-
-// * write a program given number print "sanju" if divisible by 3 and print "Geeta" if divisible by 5
-// * print "Sanju weds Geeta" if divisible by 3 and 5 both
-
-let num = 0;
-
-if (num % 3 == 0 && num % 5 == 0) {
-  console.log("Sanju weds Geeta");
-} else if (num % 3 == 0) {
-  console.log("Sanju");
-} else if (num % 5 == 0) {
-  console.log("Geeta");
-} else {
-  console.log("Number is not divisible by 3 and 5");
-}
