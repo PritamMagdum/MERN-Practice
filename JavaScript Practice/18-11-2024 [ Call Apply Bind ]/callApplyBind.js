4b6a6f57d17c3297b398c4ada45f38ee0f8347cb
Pritam <pritammagdum24@gmail.com>
2024-11-21 20:21:28 +0530

    Call Apply Bind Methods

--- JavaScript Practice/18-11-2024 [ Call Apply Bind ]/callApplyBind.js
+++ JavaScript Practice/18-11-2024 [ Call Apply Bind ]/callApplyBind.js
@@ -1,9 +1,63 @@
 // * call()
+{
+  let obj = {
+    id: 101,
+    name: "Pritam",
+    add: "Banglore",
+  };
 
-let obj = {
-  id: 101,
-  name: "Pritam",
-  add: "Banglore",
-};
+  // console.log(obj);
 
-console.log(obj);
+  function demo() {
+    console.log(this);
+    console.log(this.id);
+    console.log(this.name);
+  }
+
+  // demo(); =====> it will give you error
+
+  // call()
+  // demo.call(obj);
+}
+
+// * apply()
+{
+  let obj = {
+    id: 101,
+    name: "Pritam",
+    add: "Banglore",
+  };
+
+  // console.log(obj);
+
+  function demo(a, b, c) {
+    console.log(this);
+    console.log(this.id);
+    console.log(this.name);
+    console.log(a);
+    console.log(b);
+    console.log(c);
+  }
+
+  // demo(); =====> it will give you error
+
+  // apply()
+  // demo.apply(obj, [10, 20, 30]);
+}
+
+// * bind()
+{
+  const person = {
+    name: "Alice",
+    greet: function () {
+      console.log("Hello, " + this.name);
+    },
+  };
+
+  let obj = {
+    name: "Pritam",
+  };
+  const greetFunc = person.greet.bind(obj);
+
+  greetFunc(); // Outputs: "Hello, Alice!"
+}
