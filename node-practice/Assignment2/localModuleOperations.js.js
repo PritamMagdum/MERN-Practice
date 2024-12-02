47a1f23a41c5bbedb2207a60a22a78f3238b99a2
Pritam <pritammagdum24@gmail.com>
2024-12-02 22:58:23 +0530

    Learned Local Module Operations

---
+++ node-practice/Assignment2/localModuleOperations.js.js
@@ -0,0 +1,11 @@
+function power(num, pow) {
+  return num ** pow;
+}
+
+function squareRoot(num) {}
+
+function modulus(num, mod) {
+  return num % mod;
+}
+
+module.exports = { power, squareRoot, modulus };
