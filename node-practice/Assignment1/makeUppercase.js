51784da427254cdb559f70e6c32b12940bebe9a8
Pritam <pritammagdum24@gmail.com>
2024-12-03 22:56:19 +0530

    Solved Make UpperCase Problem

---
+++ node-practice/Assignment1/makeUppercase.js
@@ -0,0 +1,11 @@
+function makeUppercase(str) {
+  str = str.trim();
+  for(let  i =0;i<str.length;i++){
+    let char = str.charCodeAt(i);
+    let newStr = String.
+  }
+
+  return str;
+}
+
+module.exports.makeUppercase = makeUppercase;
