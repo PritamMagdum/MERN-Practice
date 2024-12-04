3f906dc73850c6af6e0f55091a76e6244b83129d
Pritam <pritammagdum24@gmail.com>
2024-12-04 23:01:50 +0530

    Learned OS Module in Core Modules Node.js

---
+++ node-practice/Core Modules/os.js
@@ -0,0 +1,14 @@
+let os = require("os");
+
+console.log(os.arch());
+console.log(os.cpus().length);
+console.log(os.freemem());
+console.log(os.hostname());
+console.log(os.machine());
+console.log(os.tmpdir());
+console.log(os.homedir());
+console.log(os.release());
+console.log(os.userInfo());
+console.log(os.platform());
+console.log(os.version());
+console.log(os.EOL);
