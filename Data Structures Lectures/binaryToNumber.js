df449fb3ea143577a1750c6786edb87ee95acba9
Pritam <pritammagdum24@gmail.com>
2024-12-07 22:50:05 +0530

    Solved Binary To Number

--- Data Structures Lectures/binaryToNumber.js
+++ Data Structures Lectures/binaryToNumber.js
@@ -1,13 +1,15 @@
 function binaryToNumber(num) {
   let count = 0;
   let sum = 0;
-  do {
-    let rem = num % 10;
-    sum += rem * 2 ** count;
-    count++;
-  } while (num < 0);
+  // Iterate as long as num is greater than 0
+  while (num > 0) {
+    let rem = num % 10; // Get the last digit of the binary number
+    sum += rem * 2 ** count; // Add the value of the digit to sum
+    count++; // Move to the next position
+    num = Math.floor(num / 10); // Remove the last digit
+  }
   return sum;
 }
 
 let res = binaryToNumber(1001010);
-console.log(res);
+console.log(res); // Output will be 74
