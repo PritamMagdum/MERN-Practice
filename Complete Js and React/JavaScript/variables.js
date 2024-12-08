3e7d9e5d80ce6ff1aa38441012dc9120844b5af3
Pritam <pritammagdum24@gmail.com>
2024-12-08 22:51:38 +0530

    Variables in JS

---
+++ Complete Js and React/JavaScript/variables.js
@@ -0,0 +1,19 @@
+// let var const
+
+// var a = 10;
+// var a = 40;
+// var a;
+// a = 90;
+
+// let
+// let a = 89;
+// let a = 89
+
+// console.log(a);
+
+// const
+// const a = 10
+// const a = 10
+
+console.log("twinkle");
+console.log("twinkle");
